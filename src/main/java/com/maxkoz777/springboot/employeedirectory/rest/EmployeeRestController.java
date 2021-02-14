9a5425840f9e14f5ab710f7400c4a7097a391bae
Maxim <maxkozhinov777@gmail.com>
2021-02-14 23:18:23 +0300

    added service layer

--- src/main/java/com/maxkoz777/springboot/employeedirectory/rest/EmployeeRestController.java
+++ src/main/java/com/maxkoz777/springboot/employeedirectory/rest/EmployeeRestController.java
@@ -2,6 +2,7 @@ package com.maxkoz777.springboot.employeedirectory.rest;
 
 import com.maxkoz777.springboot.employeedirectory.dao.EmployeeDAO;
 import com.maxkoz777.springboot.employeedirectory.entity.Employee;
+import com.maxkoz777.springboot.employeedirectory.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,17 +14,17 @@ import java.util.List;
 @RequestMapping("/api")
 public class EmployeeRestController {
 
-    private EmployeeDAO employeeDAO;
+    private EmployeeService employeeService;
 
     @Autowired
-    public EmployeeRestController(EmployeeDAO employeeDAO) {
-        this.employeeDAO = employeeDAO;
+    public EmployeeRestController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
     }
 
     @GetMapping("/employees")
     public List<Employee> findAll() {
 
-        return employeeDAO.findAll();
+        return employeeService.findAll();
 
     }
 
---
+++ src/main/java/com/maxkoz777/springboot/employeedirectory/service/EmployeeServiceImpl.java
@@ -0,0 +1,48 @@
+package com.maxkoz777.springboot.employeedirectory.service;
+
+import com.maxkoz777.springboot.employeedirectory.dao.EmployeeDAO;
+import com.maxkoz777.springboot.employeedirectory.entity.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService {
+
+    EmployeeDAO employeeDAO;
+
+    @Autowired
+    public EmployeeServiceImpl(EmployeeDAO employeeDAO) {
+        this.employeeDAO = employeeDAO;
+    }
+
+    @Override
+    @Transactional
+    public List<Employee> findAll() {
+        return employeeDAO.findAll();
+    }
+
+    @Override
+    @Transactional
+    public Employee findById(int id) {
+        return employeeDAO.findById(id);
+    }
+
+    @Override
+    @Transactional
+    public void save(Employee employee) {
+
+        employeeDAO.save(employee);
+
+    }
+
+    @Override
+    @Transactional
+    public void deleteById(int id) {
+
+        employeeDAO.deleteById(id);
+
+    }
+}
