345f318fb3ab629da603fa383d1b0e77c1c10c94
Maxim <maxkozhinov777@gmail.com>
2021-02-14 23:45:21 +0300

    all CRUD operations work properly

--- src/main/java/com/maxkoz777/springboot/employeedirectory/rest/EmployeeRestController.java
+++ src/main/java/com/maxkoz777/springboot/employeedirectory/rest/EmployeeRestController.java
@@ -4,9 +4,7 @@ import com.maxkoz777.springboot.employeedirectory.dao.EmployeeDAO;
 import com.maxkoz777.springboot.employeedirectory.entity.Employee;
 import com.maxkoz777.springboot.employeedirectory.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -25,6 +23,54 @@ public class EmployeeRestController {
     public List<Employee> findAll() {
 
         return employeeService.findAll();
+
+    }
+
+    @GetMapping("/employees/{id}")
+    public Employee getEmployee(@PathVariable int id) {
+
+        Employee employee = employeeService.findById(id);
+
+        if (employee == null) {
+            throw new RuntimeException("No such employee with id = " + id);
+        }
+
+        return employee;
+
+    }
+
+    @PostMapping("/employees")
+    public Employee addEmployee(@RequestBody Employee employee) {
+
+        employee.setId(0);
+
+        employeeService.save(employee);
+
+        return employee;
+
+    }
+
+    @PutMapping("/employees")
+    public Employee updateEmployee(@RequestBody Employee employee) {
+
+        employeeService.save(employee);
+
+        return employee;
+
+    }
+
+    @DeleteMapping("/employees/{id}")
+    public String deleteEmployee(@PathVariable int id) {
+
+        Employee employee = employeeService.findById(id);
+
+        if (employee == null) {
+            throw new RuntimeException("No such employee with id = " + id);
+        }
+
+        employeeService.deleteById(id);
+
+        return "deleted employee with id: " + id;
 
     }
 
