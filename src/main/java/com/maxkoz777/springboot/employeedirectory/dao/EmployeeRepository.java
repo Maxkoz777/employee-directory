12282d10fb72f71ef613fec435ec0817531722b8
Maxim <maxkozhinov777@gmail.com>
2021-02-16 20:58:37 +0300

    changed some properties for resource path

--- src/main/java/com/maxkoz777/springboot/employeedirectory/dao/EmployeeRepository.java
+++ src/main/java/com/maxkoz777/springboot/employeedirectory/dao/EmployeeRepository.java
@@ -2,6 +2,9 @@ package com.maxkoz777.springboot.employeedirectory.dao;
 
 import com.maxkoz777.springboot.employeedirectory.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.stereotype.Repository;
 
+@RepositoryRestResource(path = "members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 }
