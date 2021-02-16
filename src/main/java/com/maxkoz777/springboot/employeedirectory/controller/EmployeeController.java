f24235b82a21e9b29c087d71dbdc4072be2650a6
Maxim <maxkozhinov777@gmail.com>
2021-02-16 23:07:22 +0300

    included ability to add new employee

--- src/main/java/com/maxkoz777/springboot/employeedirectory/controller/EmployeeController.java
+++ src/main/java/com/maxkoz777/springboot/employeedirectory/controller/EmployeeController.java
@@ -2,15 +2,13 @@ package com.maxkoz777.springboot.employeedirectory.controller;
 
 import com.maxkoz777.springboot.employeedirectory.entity.Employee;
 import com.maxkoz777.springboot.employeedirectory.service.EmployeeService;
-import com.maxkoz777.springboot.employeedirectory.service.EmployeeServiceImpl;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import javax.annotation.PostConstruct;
-import java.util.ArrayList;
 import java.util.List;
 
 @Controller
@@ -19,20 +17,44 @@ public class EmployeeController {
 
     private EmployeeService employeeService;
 
-    @Autowired
-    public EmployeeController(EmployeeService employeeService) {
-        this.employeeService = employeeService;
+    public EmployeeController(EmployeeService theEmployeeService) {
+        employeeService = theEmployeeService;
     }
+
+    // add mapping for "/list"
 
     @GetMapping("/list")
-    public String listEmployees(Model model) {
+    public String listEmployees(Model theModel) {
 
-        List<Employee> employees = employeeService.findAll();
+        // get employees from db
+        List<Employee> theEmployees = employeeService.findAll();
 
-        model.addAttribute("employees", employees);
+        // add to the spring model
+        theModel.addAttribute("employees", theEmployees);
 
-        return "list-employees";
-
+        return "employees/list-employees";
     }
+
+    @GetMapping("/showFormForAdd")
+    public String showFormForAdd(Model theModel) {
+
+        // create model attribute to bind form data
+        Employee theEmployee = new Employee();
+
+        theModel.addAttribute("employee", theEmployee);
+
+        return "employees/employee-form";
+    }
+
+    @PostMapping("/save")
+    public String saveEmployee(@ModelAttribute("employee") Employee theEmployee) {
+
+        // save the employee
+        employeeService.save(theEmployee);
+
+        // use a redirect to prevent duplicate submissions
+        return "redirect:/employees/list";
+    }
+
 
 }
--- src/main/java/com/maxkoz777/springboot/employeedirectory/dao/EmployeeRepository.java
+++ src/main/java/com/maxkoz777/springboot/employeedirectory/dao/EmployeeRepository.java
@@ -4,6 +4,11 @@ import com.maxkoz777.springboot.employeedirectory.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+import java.util.List;
+
 //@RepositoryRestResource(path = "members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+
+    public List<Employee> findAllByOrderByLastNameAsc();
+
 }
